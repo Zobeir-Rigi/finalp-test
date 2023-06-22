c5276c7415c39fa966a3dfe06a678476a9761d6a
Zobeir-Rigi <rigi.zobir@yahoo.com>
2023-06-22 12:47:12 +0100

    searching user and finding rank and prequests

--- client/src/components/Milestone.jsx
+++ client/src/components/Milestone.jsx
@@ -1,31 +1,48 @@
 import React from "react";
 import { useState } from "react";
 import "../styles/Milestone.css";
+
 const Milestone = () => {
   const [username, setUserName] = useState(""); //Input
-  const [userCodwarsInfo, setUserCodewarsInfo] = useState([]);
-  const [data, setData] = useState([]);
+  const [CodewarsRank, setCodewarsRank] = useState([]);
+  const [pullRequestNumber, setPullRequestNumber] = useState([]);
+
   const handleUserName = (event) => {
     setUserName(event.target.value);
   };
   const codeWarsUrl = `https://www.codewars.com/api/v1/users/${username}`; //get specific user data from codewars
   const githubUrl = `https://api.github.com/search/issues?q=is:pr%20author:${username}%20user:codeyourfuture`; //get specific user data from Github
+  /* search if the user name exist and then find the codewars rank and the number of pull requests*/
+  const handleSearch = () => {
+    fetchCodeWarsRank();
+    fetchPullRequestCount();
+  };
 
-  /********************************* Fetching User Codewars Account data */
-  const handleFetchData = () => {
+  /*********************************Function => Fetching User Codewars Account data */
+  const fetchCodeWarsRank = () => {
     fetch(codeWarsUrl)
       .then((res) => res.json())
       .then((data) => {
-        setUserCodewarsInfo(data);
-        console.log(data);
+        console.log("Codewars date", data);
+        const codewarsRankNumber = data.ranks.overall.name;
+        if (codewarsRankNumber) {
+          console.log("I find Codewars Rank in the Data ...");
+          setCodewarsRank(codewarsRankNumber);
+        }
       })
       .catch((err) => console.log("something is wrong", err));
-    /********************************* Fetching User Github Account data */
+  };
+
+  /*********************************Function => Fetching User Github Account data */
+  const fetchPullRequestCount = () => {
     fetch(githubUrl)
       .then((res) => res.json())
       .then((data) => {
-        setData(data.items);
-        console.log("Data.Item is = ", data);
+        console.log("Pull requestes data", data);
+        if (data.total_count) {
+          console.log("The Number of Pull Request is = ", data.total_count);
+          setPullRequestNumber(data.total_count);
+        }
       })
       .catch((err) => console.log("something is wrong", err));
   };
@@ -34,59 +51,30 @@ const Milestone = () => {
     <div className="MileStone">
       <h3>Milestone</h3>
       <div className="input-button">
-        <input type="text" value={username} onChange={handleUserName} placeholder="Enter Your Github User Name ..."/>
-        <button onClick={handleFetchData}>Check Your Milestone</button>
+        <input
+          type="text"
+          value={username}
+          onChange={handleUserName}
+          placeholder="Enter Your Github User Name ..."
+        />
+        <button onClick={handleSearch}>Search</button>
       </div>
       <div className="table-box">
-      <table >
-        <thead>
-          <tr>
-            <th >Pulls</th>
-            <th >Codewars</th>
-            <th >Codility</th>
-            <th >Performance</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{data.length}</td>
-            <td>
-              {userCodwarsInfo.ranks &&
-                userCodwarsInfo.ranks.overall &&
-                userCodwarsInfo.ranks.overall.name}
-            </td>
-            <td>---</td>
-            
-              {data.length > 22 &&
-              userCodwarsInfo.ranks.overall.name == "5 kyu" ? (
-                <td style={{ backgroundColor: "green" }}>At Milestone</td>
-              ) : (
-                <td style={{ backgroundColor: "red" }}>Behind Milestone</td>
-              )}
-            
-          </tr>
-        </tbody>
-      </table>
+        <table>
+          <thead>
+            <tr>
+              <th>Pulls</th>
+              <th>Codewars</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{pullRequestNumber}</td>
+              <td>{CodewarsRank}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      {/* {userCodwars && (
-        <div className="generate-score">
-          <span>Name : {userCodwars.name}</span>
-          <span>Honor : {userCodwars.honor}</span>
-          <span>Clan :{userCodwars.clan}</span>
-          <span>
-            Rank :
-            {userCodwars.ranks &&
-              userCodwars.ranks.overall &&
-              userCodwars.ranks.overall.name}
-          </span>
-        </div>
-      )}
-
-      <ul>
-        {data.map((pull) => (
-          <li>{pull.url}</li>
-        ))}
-      </ul> */}
     </div>
   );
 };
