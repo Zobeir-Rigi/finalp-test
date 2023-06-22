d74125fc11d0da432ea1c64789c117d8fdd8fa00
Zobeir-Rigi <rigi.zobir@yahoo.com>
2023-06-22 13:29:51 +0100

    converted user name to search input

--- client/src/components/Milestone.jsx
+++ client/src/components/Milestone.jsx
@@ -3,44 +3,45 @@ import { useState } from "react";
 import "../styles/Milestone.css";
 
 const Milestone = () => {
-  const [username, setUserName] = useState(""); //Input
+  const [searchInput, setSearchInput] = useState(""); // which is the user name
   const [CodewarsRank, setCodewarsRank] = useState([]);
   const [pullRequestNumber, setPullRequestNumber] = useState([]);
 
-  const handleUserName = (event) => {
-    setUserName(event.target.value);
+  const handleSearchInput = (event) => {
+    console.log(event.target.value);
+    setSearchInput(event.target.value);
   };
-  const codeWarsUrl = `https://www.codewars.com/api/v1/users/${username}`; //get specific user data from codewars
-  const githubUrl = `https://api.github.com/search/issues?q=is:pr%20author:${username}%20user:codeyourfuture`; //get specific user data from Github
+  const codeWarsUrl = `https://www.codewars.com/api/v1/users/${searchInput}`; //get specific user data from codewars
+  const githubUrl = `https://api.github.com/search/issues?q=is:pr%20author:${searchInput}%20user:codeyourfuture`; //get specific user data from Github
   /* search if the user name exist and then find the codewars rank and the number of pull requests*/
-  const handleSearch = () => {
+  const handleSearch = () =>{
     fetchCodeWarsRank();
     fetchPullRequestCount();
-  };
+  }
 
   /*********************************Function => Fetching User Codewars Account data */
   const fetchCodeWarsRank = () => {
     fetch(codeWarsUrl)
       .then((res) => res.json())
       .then((data) => {
-        console.log("Codewars date", data);
-        const codewarsRankNumber = data.ranks.overall.name;
-        if (codewarsRankNumber) {
-          console.log("I find Codewars Rank in the Data ...");
-          setCodewarsRank(codewarsRankNumber);
-        }
+        console.log("Codewars date" , data)
+           const codewarsRankNumber = data.ranks.overall.name;
+        if( codewarsRankNumber ) {
+            console.log("I find Codewars Rank in the Data ...")
+            setCodewarsRank(codewarsRankNumber);
+        } 
       })
-      .catch((err) => console.log("something is wrong", err));
-  };
+      .catch((err) => console.log("something is wrong", err))
+    };
 
-  /*********************************Function => Fetching User Github Account data */
-  const fetchPullRequestCount = () => {
-    fetch(githubUrl)
+    /*********************************Function => Fetching User Github Account data */
+    const fetchPullRequestCount = () => {
+      fetch(githubUrl)
       .then((res) => res.json())
       .then((data) => {
-        console.log("Pull requestes data", data);
-        if (data.total_count) {
-          console.log("The Number of Pull Request is = ", data.total_count);
+        console.log("Pull requestes data", data)
+        if ( data.total_count ) {
+          console.log("The Number of Pull Request is = ",data.total_count )
           setPullRequestNumber(data.total_count);
         }
       })
@@ -51,10 +52,11 @@ const Milestone = () => {
     <div className="MileStone">
       <h3>Milestone</h3>
       <div className="input-button">
+        
         <input
           type="text"
-          value={username}
-          onChange={handleUserName}
+          value={searchInput}
+          onChange={handleSearchInput}
           placeholder="Enter Your Github User Name ..."
         />
         <button onClick={handleSearch}>Search</button>
@@ -75,6 +77,7 @@ const Milestone = () => {
           </tbody>
         </table>
       </div>
+      
     </div>
   );
 };
